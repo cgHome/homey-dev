324050d03adff2e6169e9d6c13045c59cdf39195
Chris (cFlat-inc) <chris@MacBook-Pro.local>
2018-10-04 23:25:43 +0200

    homey-run

--- start.sh
+++ start.sh
@@ -8,16 +8,23 @@ docker build -t homey-dev "homey-dev/"
 docker run -d -ti -v ${PWD}:/app -p 9229:9229 --rm --name homey-dev homey-dev
 docker exec -ti homey-dev sh -c "athom login"
 
-# Define homey bash-function
+# Define homey bash-functions
 # Mac OS X .bashrc not working, see: https://superuser.com/a/244990
+# To delete run: unset -f homey homey-run && rm ~/.bashrc OR removed on nano ~/.bashrc
 source ~/.bashrc
-if [ -n "$(type -t homey)" ] && [ "$(type -t homey)" = function ];
-    then 
-        echo "homey: bash-function already exist (To delete run: unset -f homey homey-run && rm ~/.bashrc OR nano ~/.bashrc)";
-    else 
-        CMD='homey() { ARGS=${@}; docker exec -ti homey-dev sh -c "$ARGS"; }'
-        echo "$CMD" >> ~/.bashrc
-        CMD='homey-run() { ARGS=${@}; homey("npm run-script $ARGS --prefix homey-dev") }'
-        echo "$CMD" >> ~/.bashrc
-        echo "homey: bash-function added (!Attention! Restart the current terminal-session or run: source ~/.bashrc)";
+if [ -n "$(type -t homey)" ] && [ "$(type -t homey)" = function ]; then 
+    echo "homey: bash-function already exist";
+else 
+    CMD='homey() { ARGS=${@}; docker exec -ti homey-dev sh -c "$ARGS"; }'
+    echo "$CMD" >> ~/.bashrc
+    source ~/.bashrc
+    echo "homey: bash-function added (!Attention! Restart the current terminal-session or run: source ~/.bashrc)";
+fi
+if [ -n "$(type -t homey-run)" ] && [ "$(type -t homey-run)" = function ]; then 
+    echo "homey-run bash-function already exist";
+else 
+    CMD='homey-run() { ARGS=${@}; homey npm run-script "$ARGS" --prefix homey-dev; }'
+    echo "$CMD" >> ~/.bashrc
+    source ~/.bashrc
+    echo "homey: bash-function added (!Attention! Restart the current terminal-session or run: source ~/.bashrc)";
 fi
